a4c3f805ce657cf083b6b1dfe3563021d53726fd
Iamnerses <100987490+Iamnerses@users.noreply.github.com>
2023-04-02 18:43:53 +0400

    Add files via upload

---
+++ prob_div.py
@@ -0,0 +1,15 @@
+#Տարբերակ 1
+N = int(input())
+data = [int(x) for x in input().split()]
+list = []
+for i in data:
+    rem == i%10
+    if rem
+
+#Տարբերակ 2
+N = int(input())
+data = [int(x) for x in input().split()]
+if data[-1]%10==0:
+    print("Yes")
+else:
+    print("No")
