739130ad0db4a0d2a75bbfeb53e00d4be294e000
NersesSa <100987490+Iamnerses@users.noreply.github.com>
2023-04-03 22:46:44 +0400

    Update prob_div.py

--- prob_div.py
+++ prob_div.py
@@ -1,16 +1,3 @@
-#Տարբերակ 1
-N = int(input())
-data = [int(x) for x in input().split()]
-list = []
-for i in data:
-    rem == i%10
-    list.append(rem)
-    if list[-1]==0
-        print("Yes")
-    else:
-        print("No")
-
-#Տարբերակ 2
 N = int(input())
 data = [int(x) for x in input().split()]
 if data[-1]%10==0:
