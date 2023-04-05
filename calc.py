439f2784c9e77efebcb79b6ba895335da7818555
NersesSa <100987490+Iamnerses@users.noreply.github.com>
2023-04-05 14:14:52 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,34 +1,38 @@
-class calc: 
-    def sum(x,y):
+class calc(): 
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def sum(self):
         return x+y  
-    def div(x,y):
+    def div(self):
         return x/y
-    def sub(x,y):
+    def sub(self):
         return x-y
-    def mul(x,y):
+    def mul(self):
         return x*y
+operators = ["+","-","*","/"]
 
 class IOS_calc(calc):
-    def power(x,y):
+    def power(self):
         return x**y
 
 class Android_calc(calc):
-    def mod(x,y):
+    def mod(self):
         return x%y
 
+while True:
 
+    print(">>> WELCOME TO CALCULATOR <<<\n\nCalculator has 4 simple functions by default \n>Addition\n>Substraction\n>Multiplication\n>Division\n\n>>>")    
+    calc_verse = input("Please enter calculator version to get more functions(IOS or Android):")
+    if calc_verse.capitalize() == str("Ios"):
+        print("You have chosed IOS\n\nHere is IOS calculator additional function\n>>>\n\n>Exponentiation(Raise to power)\n\n")
+        x=int(input("Enter first number: "))
+        y=int(input("Enter second number: "))
+        oper = input("Enter operator(+,-,*,/)")
+        if oper == "+":        
+           print( calc.sum())
 
-print(">>>\n\nCalculator has 4 simple functions by default \n>Addition\n>Substraction\n>Multiplication\n>Division\n\n>>>")    
-calc_verse = input("Please enter calculator version to get more functions(IOS or Android):")
-if calc_verse.capitalize() == str("Ios"):
-    print("You have chosed IOS\n\nHere are IOS calculator additional function\n>>>\n\n>Exponentiation(Raise to power)\n\n")
- 
-
-elif calc_verse.capitalize() == str("Android"):
-    print("You have chosed Android\n\nHere are Android calculator functions\n>>>\n\n>Modulo")
-    while True:
-        
-       cont=str(input("Enter Yes to quit"))
-       break:
-else:
-    print("Wrong input try again")
+    elif calc_verse.capitalize() == str("Android"):
+        print("You have chosed Android\n\nHere is Android calculator function\n>>>\n\n>Modulo division\n\n")
+    else:
+        print("Wrong input try again")
